e09a09f46fc86888caf28171854dd0ca59059d1f
Karthick Manoharan <mkkrthck@gmail.com>
2019-10-29 01:21:52 -0400

    change setupProxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(proxy('/spi', {
-        target: 'http://localhost:9001',
+        target: 'http://localhost:9002',
         pathRewrite: {
             '^/spi': ''
         }
